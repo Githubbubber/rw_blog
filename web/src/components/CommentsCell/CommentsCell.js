6c3a80c14931c5a2084d15d6110ad381b26bc375
Meke Brown <mekesiabrown@gmail.com>
2022-09-18 17:08:25 -0400

    refactor: update comments code

--- web/src/components/CommentsCell/CommentsCell.js
+++ web/src/components/CommentsCell/CommentsCell.js
@@ -1,11 +1,12 @@
 import Comment from 'src/components/Comment'
 
 export const QUERY = gql`
-  query CommentsQuery {
-    comments {
+  query CommentsQuery($postId: Int!) {
+    comments(postId: $postId) {
       id
       name
       body
+      postId
       createdAt
     }
   }
@@ -13,7 +14,9 @@ export const QUERY = gql`
 
 export const Loading = () => <div>Loading...</div>
 
-export const Empty = () => <div>Empty</div>
+export const Empty = () => {
+  return <div className="text-center text-gray-500">No comments yet</div>
+}
 
 export const Failure = ({ error }) => (
   <div style={{ color: 'red' }}>Error: {error?.message}</div>
